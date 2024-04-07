1aabe3a5f6d1aab99569ae29cd7f1e7d2c6c5eff
Wanderson Ferreira <wandersonbferreira@gmail.com>
2024-04-07 17:01:08 -0300

    move pipes

--- src/app/shared/pipes/pokemon-type/pokemon-type.pipe.ts
+++ src/app/shared/pipes/pokemon-type/pokemon-type.pipe.ts
@@ -1,5 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { PokemonType } from '../models/pokemon-type';
+import { PokemonType } from '../../models/pokemon-type';
 
 @Pipe({
   name: 'pokemonType',
